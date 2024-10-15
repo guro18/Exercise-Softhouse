9a5dfa447ab5beee5c4f97b3562a79187a8debf1
guro18 <rollgustav95@gmail.com>
2024-10-15 14:57:49 +0200

    added functionality for convertion

--- XMLConverter.ts
+++ XMLConverter.ts
@@ -2,8 +2,15 @@ import * as fs from 'fs';
 
 const args = process.argv;
 const inputFile = args[2];
+const outputFile = args[3];
 const data = fs.readFileSync(inputFile, 'utf8');
-console.log(data);
+
+const tagsDict: { [key: string]: string[] } = {
+    P: ['person', 'firstname', 'lastname'],
+    T: ['phone', 'mobile', 'landlinenumber'],
+    A: ['address', 'street', 'city', 'postalcode'],
+    F: ['family', 'name', 'born']
+  };
 
 class XMLConverter {
     private delimiter: string;
@@ -19,8 +26,32 @@ class XMLConverter {
         .filter(row => row.trim() !== '')
         .map(row => row.split(this.delimiter).map(field => field.trim()));
     }
-  }
+
+    generateXML(rows: string[][]): string {
+        let xml = '<people>\n';
+
+        rows.forEach(row => {
+            const recordType = row[0];
+            const tags = tagsDict[recordType];
+
+            if (tags) {
+                xml += `  <${tags[0]}>\n`;
+                for (let i = 1; i < tags.length; i++) {
+                    xml += `    <${tags[i]}>${row[i]}</${tags[i]}>\n`;
+                }
+                xml += `  </${tags[0]}>\n`;
+            }
+        });
+        xml += '</people>';
+        return xml;
+    }
+  };
   
   const converter = new XMLConverter();
+  console.log(data);
   const rows = converter.parseRows(data);
   console.log(rows);
+  console.log(tagsDict);
+  const xmlFile = converter.generateXML(rows);
+  console.log(xmlFile);
+  fs.writeFileSync(outputFile, xmlFile);
